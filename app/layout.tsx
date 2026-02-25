3d846f757dd967d355b28ba0cac3cf234d226781
ashutoshShukla12 <ashukla0611@conestogac.on.ca>
2026-02-25 12:37:06 -0500

    analytics1.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
@@ -68,6 +67,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
